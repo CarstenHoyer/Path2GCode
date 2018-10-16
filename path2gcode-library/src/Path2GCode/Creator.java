dd4cc00b6694064d5e44e26833db518fa48e44ff
Carsten Høyer <carsten@reload.dk>
2018-10-16 21:08:27 +0200

    Make Creator field protected

--- path2gcode-library/src/Path2GCode/Creator.java
+++ path2gcode-library/src/Path2GCode/Creator.java
@@ -3,11 +3,11 @@ package Path2GCode.library;
 import java.util.ArrayList;
 
 public class Creator {
-  ArrayList<Path> paths = new ArrayList<Path>();
-  Device printer;
-  Settings settings;
+  protected ArrayList<Path> paths = new ArrayList<Path>();
+  protected Device printer;
+  protected Settings settings;
   public Creator(Device t_printer, Settings t_settings) {
     printer = t_printer;
     settings = t_settings;
   }
-}
+}	
