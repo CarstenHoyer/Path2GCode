4535c6611d2f02eda150dd465e31450b6b516eea
Carsten Høyer <carsten@reload.dk>
2018-10-16 21:10:24 +0200

    Adjust Cube and use new Path rotation

--- path2gcode-library/src/Path2GCode/Cube.java
+++ path2gcode-library/src/Path2GCode/Cube.java
@@ -13,17 +13,18 @@ public class Cube extends Creator {
   }
   
   public void generate(float c_x, float c_y, float length_side_cube, float rotation) {
+	float halv_length = length_side_cube / 2;
     paths = new ArrayList<Path>();
     Path path = new Path();
-    PVector p1 = new PVector(c_x, c_y);
+    PVector p1 = new PVector(c_x - halv_length, c_y - halv_length);
     path.add(p1);
-    path.add(new PVector(c_x + length_side_cube, c_y));
-    path.add(new PVector(c_x + length_side_cube, c_y + length_side_cube));
-    path.add(new PVector(c_x, c_y + length_side_cube));
+    path.add(new PVector(c_x + halv_length, c_y - halv_length));
+    path.add(new PVector(c_x + halv_length, c_y + halv_length));
+    path.add(new PVector(c_x - halv_length, c_y + halv_length));
     path.add(p1);
     paths.add(path);
     if (PApplet.abs(rotation) > 0) {
-      path.rotate(rotation, c_x, c_y, length_side_cube);
+      path.rotate(rotation, c_x, c_y);
     }
   }
 }
